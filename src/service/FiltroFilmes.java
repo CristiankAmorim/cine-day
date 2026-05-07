52e99dcacf90c3b23f749902a6959cdc828b879f
Cristian Amorim <camorim1100@gmail.com>
2026-05-07 20:21:26 -0300

    Criando o método para filtrar a classificação do filme

--- src/service/FiltroFilmes.java
+++ src/service/FiltroFilmes.java
@@ -5,7 +5,13 @@ import model.PerfilCinefilo;
 
 public class FiltroFilmes {
 
+	
+	
 	private boolean naoFoiAssistido(Filme filme, PerfilCinefilo perfil) {
 		return !perfil.filmeJaAssistido(filme);
+	}
+	
+	private boolean classificacaoAceitavel(Filme filme, PerfilCinefilo perfil) {
+		return filme.getClassificacao().dentroDaClassificacaoMaxima(perfil.getClassificacaoMaxima());
 	}
 }
