5241e5c73e59297e3efff9ca078f467895557384
Cristian Amorim <camorim1100@gmail.com>
2026-05-07 20:47:28 -0300

    Criando o método público responsável pela filtragem dos filmes

--- src/service/FiltroFilmes.java
+++ src/service/FiltroFilmes.java
@@ -1,12 +1,32 @@
 package service;
 
+import java.util.Collections;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import model.Filme;
 import model.PerfilCinefilo;
 import model.enums.Genero;
 
 public class FiltroFilmes {
 
-	
+	/**
+	 * Filtrar os filmes do catálogo, removendo os que violam alguma regra do perfil.
+	 * @param filmes
+	 * @param perfil
+	 * @return
+	 */
+	public Set<Filme> filtrar(Set<Filme> filmes, PerfilCinefilo perfil) {
+		if(filmes.isEmpty() || filmes == null) {
+			return Collections.emptySet();
+		}
+		return (Set<Filme>) filmes.stream()
+				.filter(filme -> naoFoiAssistido(filme, perfil))
+				.filter(filme -> classificacaoAceitavel(filme, perfil))
+				.filter(filme -> idiomasAceitos(filme, perfil))
+				.filter(filme -> nenhumGeneroBloqueado(filme, perfil))
+				.collect(Collectors.toSet());
+	}
 	
 	private boolean naoFoiAssistido(Filme filme, PerfilCinefilo perfil) {
 		return !perfil.filmeJaAssistido(filme);
