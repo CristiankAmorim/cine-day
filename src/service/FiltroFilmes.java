2397ed83f6bc2e73b1d8bc372ea966e29594a325
Cristian Amorim <camorim1100@gmail.com>
2026-05-07 20:37:31 -0300

    Criando o método para filtrar gêneros não bloqueados

--- src/service/FiltroFilmes.java
+++ src/service/FiltroFilmes.java
@@ -2,6 +2,7 @@ package service;
 
 import model.Filme;
 import model.PerfilCinefilo;
+import model.enums.Genero;
 
 public class FiltroFilmes {
 
@@ -17,5 +18,14 @@ public class FiltroFilmes {
 	
 	private boolean idiomasAceitos(Filme filme, PerfilCinefilo perfil) {
 		return perfil.getIdiomasAceitos().isEmpty() || perfil.getIdiomasAceitos().contains(filme.getIdioma());
+	}
+	
+	private boolean nenhumGeneroBloqueado(Filme filme, PerfilCinefilo perfil) {
+		return filme.getGeneros().stream()
+				.noneMatch(genero -> isPesoBloqueado(genero, perfil));
+	}
+	
+	private boolean isPesoBloqueado(Genero genero, PerfilCinefilo perfil) {
+		return perfil.getPesoDoGenero().containsKey(genero) && perfil.getPesoDoGenero(genero) == 0.0;
 	}
 }
