0e5e5d6558718c986606515292c0f9efc3429aa8
Cristian Amorim <camorim1100@gmail.com>
2026-05-10 11:55:34 -0300

    Criando o método privado  para pontuar os filmes da lista

--- src/service/RecomendadorService.java
+++ src/service/RecomendadorService.java
@@ -2,7 +2,11 @@ package service;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+
 import model.Filme;
+import model.Recomendacao;
+import model.Usuario;
 import util.GeradorAleatorio;
 
 public class RecomendadorService {
@@ -15,6 +19,32 @@ public class RecomendadorService {
 	private CalculadoraScore calculadoraScore;
 	private FiltroFilmes filtroFilmes;
 	
+	public RecomendadorService(CatalogoFilmesAPI catalogoAPI, HistoricoUsuarioRepository historicoUsuario,
+			NotificadorPush notificadorPush, GeradorAleatorio geradorAleatorio, CalculadoraScore calculadoraScore,
+			FiltroFilmes filtroFilmes) {
+		this.catalogoAPI = catalogoAPI;
+		this.historicoUsuario = historicoUsuario;
+		this.notificadorPush = notificadorPush;
+		this.geradorAleatorio = geradorAleatorio;
+		this.calculadoraScore = calculadoraScore;
+		this.filtroFilmes = filtroFilmes;
+	}
+
+	public List<Recomendacao> recomendar(Usuario usuario, int topN) {
+		List<Filme> catalogo = buscarCatalogo();
+		
+		if(catalogo.isEmpty()) {
+			return Collections.emptyList();
+		}
+		
+		List<Filme> filmesFiltrados = filtroFilmes.filtrar(catalogo, usuario.getPerfil());
+		if(filmesFiltrados.isEmpty()) {
+			return Collections.emptyList();
+		}
+		
+		//List<Recomendacao> recomendacoes = 
+	}
+
 	private List<Filme> buscarCatalogo() {
 		try {
 			List<Filme> filmes = catalogoAPI.buscarTodos();
@@ -23,6 +53,17 @@ public class RecomendadorService {
 			System.out.println("Falha ao buscar o catálogo: " + e.getMessage());
 			return Collections.emptyList();
 		}
+	}
+
+	private List<Recomendacao> pontuarFilmes(List<Filme> filmes, Usuario usuario) {
+		return filmes.stream()
+				.map(filme -> criarRecomendacao(filme, usuario))
+				.collect(Collectors.toList());
+	}
+	
+	private Recomendacao criarRecomendacao(Filme filme, Usuario usuario) {
+		double score = calculadoraScore.calcularScore(filme, usuario.getPerfil());
+		return new Recomendacao(filme, score);
 	}
 	
 }
