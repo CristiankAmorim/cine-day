f2d20c2e6c742539109c1498704aaa3035066d63
Cristian Amorim <camorim1100@gmail.com>
2026-05-10 11:35:21 -0300

    Criando o método privado para gerar justificativa textual na classe Recomendacao

--- src/model/Recomendacao.java
+++ src/model/Recomendacao.java
@@ -1,15 +1,27 @@
 package model;
 
+import model.enums.Genero;
+
 public class Recomendacao {
 
 	private Filme filme;
 	private double score;
 	private String justificativa;
 	
-	public Recomendacao(Filme filme, double score, String justificativa) {
+	public Recomendacao(Filme filme, double score) {
 		this.filme = filme;
 		this.score = score;
-		this.justificativa = justificativa;
+		this.justificativa = gerarJustificativa(filme, score);
+	}
+	
+	private static String gerarJustificativa(Filme filme, double score) {
+		String generosTexto = filme.getGeneros().stream()
+				.map(Genero::name)
+				.reduce((a, b) -> a + " e " + b)
+				.orElse("variados");
+		return String.format(
+				"Recomendamos '%s' porque você gosta de %s e ele tem score %.0f/100.",
+				filme.getTitulo(), generosTexto, score);
 	}
 
 	public Filme getFilme() {
